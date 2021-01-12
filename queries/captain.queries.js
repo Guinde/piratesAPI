956deaf7af836991a1e2aa7566b9e5146ee4a55c
santana <johnie_93300@hotmail.fr>
2021-01-12 15:46:15 +0100

    add queries to get captain by name et list of crew

---
+++ queries/captain.queries.js
@@ -0,0 +1,38 @@
+const Captain = require("../database/models/captain.model");
+
+exports.getCaptainByName = async name => {
+  try {
+    const captain = await Captain.findOne({ name: name }).exec();
+    return captain;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
+exports.getPiratesOfCrew = async name => {
+    try {
+        const pirates = await Captain.findOne({ name: name })
+        .populate("memberOfCrew")
+        .select("memberOfCrew")
+        .exec();
+        return pirates;
+    } catch(e) {
+        throw new Error(e.message);
+    }
+}
+
+exports.createCaptain = async body => {
+    try {
+      const { name, password, age, crew } = body;
+      const hashdPwd = await Captain.hashPassword(password);
+      const captain = new Captain({
+        name,
+        age,
+        crew,
+        password: hashdPwd,
+      });
+      return captain.save();
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  };
