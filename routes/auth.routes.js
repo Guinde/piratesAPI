48ea9088ea126fd72b9a5a4403053c8f18ed4d9b
santana <johnie_93300@hotmail.fr>
2021-01-12 17:28:19 +0100

    add middlewares on all authRoutes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,25 @@
 const router = require("express").Router();
 const { login, register } = require('../controllers/auth.controllers');
+const { getCaptain } = require ('../controllers/captain.controllers');
+const { verifyToken } = require('../middlewares/jwt.middlewares');
+const { checkParamsLogin, checkParamsRegister} = require("../middlewares/security.middlewares");
 
 /**
  * @route POST /login
  * @desc Connexion with login  
  */
-router.post("/login", login);
+router.post("/login", checkParamsLogin, login);
 
 /**
  * @route POST /signup
  * @desc Create new captain 
  */
-router.post("/register", register);
+router.post("/register", checkParamsRegister, register);
+
+/**
+ * @route GET / user
+ * @desc Get user
+ */
+router.get("/user", verifyToken, getCaptain);
 
 module.exports = router;
