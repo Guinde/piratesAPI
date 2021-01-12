368c2f72ea77d241c235180d29d0fe59305dd6cf
santana <johnie_93300@hotmail.fr>
2021-01-12 18:10:48 +0100

    replace checkParamsRegister to checkParamsCaptain

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { login, register } = require('../controllers/auth.controllers');
 const { getCaptain } = require ('../controllers/captain.controllers');
 const { verifyToken } = require('../middlewares/jwt.middlewares');
-const { checkParamsLogin, checkParamsRegister} = require("../middlewares/security.middlewares");
+const { checkParamsLogin, checkParamsCaptain} = require("../middlewares/security.middlewares");
 
 /**
  * @route POST /login
@@ -14,7 +14,7 @@ router.post("/login", checkParamsLogin, login);
  * @route POST /signup
  * @desc Create new captain 
  */
-router.post("/register", checkParamsRegister, register);
+router.post("/register", checkParamsCaptain, register);
 
 /**
  * @route GET / user
