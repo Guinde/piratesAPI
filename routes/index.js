e129d0bb7b38c29c9c9fa1ddb7781ddb4e64974d
santana <johnie_93300@hotmail.fr>
2021-01-12 17:35:25 +0100

    add middleware captainRouter

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authRouter = require('./auth.routes');
+const captainRouter = require('./captain.routes');
 
 router.use("/auth", authRouter);
+routes.use("captain", captainRouter)
 
 module.exports = router;
