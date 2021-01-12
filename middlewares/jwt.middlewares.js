fb23717987f6f1495d97f84e7ecda13e5a075dcf
santana <johnie_93300@hotmail.fr>
2021-01-12 17:17:13 +0100

    add middleware to verify token

--- middlewares/jwt.middlewares.js
+++ middlewares/jwt.middlewares.js
@@ -16,3 +16,36 @@ exports.createJwtToken = (name, age, crew) => {
     );
     return jwtToken;
 };
+
+const checkExpirationToken = token => {
+    const tokenExp = token.exp;
+    const nowInSec = Math.floor(Date.now() / 1000);
+    if(nowInSec > tokenExp)
+        throw new Error("Token expired")
+}
+
+const verifyBearerToken = bearerHeader => {
+    if(bearerHeader){
+        const bearerToken = bearerHeader.split(' ');
+        if(bearerToken[0] === 'Bearer' && bearerToken[1]) 
+            return bearerToken[1];
+        else 
+            throw new Error("Not authorized")
+    } else {
+        throw new Error("Not authorized")
+    }
+}
+
+exports.verifyToken = (req, res, next) => {
+    try {
+        const bearerHeader = req.headers['authorization'];
+        const token = verifyBearerToken(bearerHeader);
+        checkExpirationToken(token);
+        const decodedToken = jwt.verify(token, env.jwtSecret, { ignoreExpiration: true });
+        req.captain = decodedToken;
+        next();
+    } catch(e) {
+        res.status(401).json({ error: e.message })
+    }
+
+}
