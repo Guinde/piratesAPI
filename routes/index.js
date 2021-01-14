d76b8d010084b467edaf6558149efdcb9bddf066
santana <johnie_93300@hotmail.fr>
2021-01-14 01:48:29 +0100

    add new middleware pirates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const authRouter = require('./auth.routes');
 const captainRouter = require('./captain.routes');
+const piratesRouter = require('./pirates.routes');
 const { verifyToken } = require('../middlewares/jwt.middlewares');
 
 router.use("/auth", authRouter);
 router.use("/captain", verifyToken, captainRouter)
+router.use("/pirates", verifyToken, piratesRouter)
 
 module.exports = router;
