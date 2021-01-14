6679ef92f272b9c81c0896c31786b2d18a202b38
santana <johnie_93300@hotmail.fr>
2021-01-14 02:11:51 +0100

    modify object user in response getCaptain

--- controllers/captain.controllers.js
+++ controllers/captain.controllers.js
@@ -7,25 +7,27 @@ const {
 } = require('../queries/captain.queries');
 
 exports.getCaptain = async (req, res) => {
-    if(req.captain) {
-        const { name, id } = req.captain;
-        const captain = await getCaptainById(id)
-        const pirates = await getPiratesOfCrew(name);
-        const { memberOfCrew } = pirates;
-        res.status(200).json({ user: captain, pirates: memberOfCrew });
-        res.end()
-    }
-    else
-        res.status(401).json({ error: "Not connected" });
+  if(req.captain) {
+      const { name, id } = req.captain;
+      const captain = await getCaptainById(id)
+      const pirates = await getPiratesOfCrew(name);
+      const { memberOfCrew } = pirates;
+      res.status(200).json({ user: {captain, memberOfCrew} });
+      res.end()
+  }
+  else
+      res.status(401).json({ error: "Not connected" });
 }
 
 exports.editCaptain = async (req, res) => {
     if (req.captain) {
         try {
-          const { id } = req.captain;
-          await modifyCaptainInfos(name, req.body);
-          const captainUpdate = await getCaptainById(id);
-          res.status(200).json({ data: captainUpdate });
+          const { id, name } = req.captain;
+          await modifyCaptainInfos(id, req.body);
+          const captain = await getCaptainById(id);
+          const pirates = await getPiratesOfCrew(name);
+          const { memberOfCrew } = pirates;
+          res.status(200).json({ data: {captain, memberOfCrew} });
         } catch (e) {
             res.status(401).json({ error: e.message })
         }
